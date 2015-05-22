56131c899a265f45ec4f7b1695a933812dbc0c54
OA Wu <comdan66@gmail.com>
2015-05-22 11:58:18 +0800

    Finish.

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -12,9 +12,28 @@ class Main extends Site_controller {
   }
 
   public function crontab () {
+    $this->load->library ('phpQuery');
+    $url = 'http://www.baishatun.com.tw/gps/';
 
+    if (!($get_html_str = str_replace ('&amp;', '&', urldecode (file_get_contents ($url)))))
+    return ;
+
+    preg_match_all ('/addMarker\s*\((?P<lat>.*)\s*,\s*(?P<lng>.*)\);/', $get_html_str, $result);
+
+    if (!($result['lat'] && $result['lng']&& $result['lat'][0] && $result['lng'][0]))
+    return ;
+
+    Path::create (array (
+        'lat' => $result['lat'][0],
+        'lng' => $result['lng'][0],
+      ));
   }
   public function index () {
+
+    foreach ($paths = Path::all () as $path) {
+      $this->add_hidden (array ('class' => 'latlng', 'data-lat' => $path->lat, 'data-lng' => $path->lng));
+    }
+
     $this->add_js ('https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&language=zh-TW', false)
          ->load_view (null);
   }
--- application/views/content/site/main/index/content.js
+++ application/views/content/site/main/index/content.js
@@ -10,6 +10,20 @@ $(function () {
                              .appendTo ($('body'));
   var $map = $('#map');
 
+  var latlngs = $('.latlng').map (function () {
+    return {lat: $(this).data ('lat'), lng: $(this).data ('lng')};
+  });
+
+  var lastLatlng = latlngs[latlngs.length - 1];
+
+  var merks = [];
+
+  function circlePath (r) {
+    return 'M 0 0 m -' + r + ', 0 '+
+           'a ' + r + ',' + r + ' 0 1,0 ' + (r * 2) + ',0 ' +
+           'a ' + r + ',' + r + ' 0 1,0 -' + (r * 2) + ',0';
+  }
+
   function initialize () {
     var option = {
         zoom: 16,
@@ -20,10 +34,32 @@ $(function () {
         zoomControl: true,
         scrollwheel: true,
         streetViewControl: true,
-        center: new google.maps.LatLng (23.568038757736595, 120.30465692281723),
+        center: new google.maps.LatLng (lastLatlng.lat, lastLatlng.lng),
       };
 
     var _map = new google.maps.Map ($map.get (0), option);
+
+    merks = latlngs.map (function (i, t) {
+      return new google.maps.Marker ({
+          map: _map,
+          draggable: false,
+          position: new google.maps.LatLng (t.lat, t.lng),
+          icon: i == latlngs.length - 1 ? 'resource/image/gps.png' : {
+            path: circlePath (10),
+            strokeColor: 'rgba(249, 39, 114, 1)',
+            strokeWeight: 1,
+            fillColor: 'rgba(249, 39, 114, .8)',
+            fillOpacity: 0.5
+          }
+        });
+    });
+
+    new google.maps.Polyline ({
+        map: _map,
+        path: merks.map (function (i, t) { return t.position; }),
+        strokeColor: 'rgba(249, 39, 114, .15)',
+        strokeWeight: 10
+      });
 
     $loading.fadeOut (function () {
       $(this).hide (function () {
@@ -31,6 +67,6 @@ $(function () {
       });
     });
   }
-  google.maps.event.addDomListener (window, 'load', initialize);
 
+  google.maps.event.addDomListener (window, 'load', initialize);
 });
