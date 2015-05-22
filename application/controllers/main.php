6d8c4f7ef39697bf35eddcbf95d329845a6ebe41
OA Wu <comdan66@gmail.com>
2015-05-22 12:02:21 +0800

    Add error log.

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -16,17 +16,24 @@ class Main extends Site_controller {
     $url = 'http://www.baishatun.com.tw/gps/';
 
     if (!($get_html_str = str_replace ('&amp;', '&', urldecode (file_get_contents ($url)))))
-    return ;
+      return ErrorLog::create (array (
+          'message' => '取不到原始碼！'
+        ));
 
     preg_match_all ('/addMarker\s*\((?P<lat>.*)\s*,\s*(?P<lng>.*)\);/', $get_html_str, $result);
 
     if (!($result['lat'] && $result['lng']&& $result['lat'][0] && $result['lng'][0]))
-    return ;
+      return ErrorLog::create (array (
+          'message' => '網頁內容有誤！'
+        ));
 
-    Path::create (array (
-        'lat' => $result['lat'][0],
-        'lng' => $result['lng'][0],
-      ));
+    if (!verifyCreateOrm ($path = Path::create (array (
+                'lat' => $result['lat'][0],
+                'lng' => $result['lng'][0],
+              ))))
+      return ErrorLog::create (array (
+          'message' => '重複！'
+        ));
   }
   public function index () {
 
