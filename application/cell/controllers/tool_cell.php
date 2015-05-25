0b28fc44b0acf4a75551e2ce782b0a61dedcbc8e
OA Wu <comdan66@gmail.com>
2015-05-25 21:50:44 +0800

    Fix code.

--- application/cell/controllers/tool_cell.php
+++ application/cell/controllers/tool_cell.php
@@ -12,19 +12,19 @@ class Tool_cell extends Cell_Controller {
     return array ('time' => 60 * 10, 'key' => $id);
   }
   public function api2 ($id) {
-
     if ($id == 0) {
       $last = ShowtaiwanPath::last ();
-      $paths = array ();
 
-      for ($i = 0; ($id = round (($i * (2 + ($i - 1) * 0.2)) / 2)) < $last->id; $i++)
-        if ($path = ShowtaiwanPath::find ('one', array ('select' => 'id, lat, lng, time_at', 'conditions' => array ('id = ?', $last->id - $id))))
-          array_push ($paths, $path);
+      $ids = array ();
+      for ($i = 0; ($id = round (($i * (2 + ($i - 1) * 0.25)) / 2)) < $last->id; $i++)
+        array_push ($ids, $last->id - $id);
 
-      $paths = array_reverse ($paths);
+      $ids = array_reverse ($ids);
+      $paths = ShowtaiwanPath::find ('all', array ('select' => 'id, lat, lng, time_at', 'order' => 'FIELD(id, ' . implode (', ', $ids) . ')', 'conditions' => array ('id IN (?)', $ids)));
 
       if(isset ($paths[count ($paths) - 1]) && ($paths[count ($paths) - 1]->id != $last->id))
         array_push ($paths, $last);
+
     } else {
       $paths = ShowtaiwanPath::find ('all', array ('select' => 'id, lat, lng, time_at', 'conditions' => array ('id > ?', $id)));
     }
