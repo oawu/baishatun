1b84b2efb7b66538dd61aed63adf1618db63f7fe
OA Wu <comdan66@gmail.com>
2016-03-17 02:04:13 +0800

    Add 2016 page

--- js/baishatun.js
+++ js/baishatun.js
@@ -3,7 +3,7 @@
  * @copyright   Copyright (c) 2015 OA Wu Design
  */
 
-var _url = 'http://dev.mazu.ioa.tw/api/baishatun/1/', _url2 = 'http://dev.mazu.ioa.tw/api/baishatun/location/';
+var _url = 'http://dev.mazu.ioa.tw/api/baishatun/1/', _url2 = 'http://dev.mazu.ioa.tw/api/baishatun/location/', _url3 = 'http://dev.mazu.ioa.tw/api/baishatun/heatmap/';
 
 function getUnit (will, now) { var addLat = will.lat () - now.lat (), addLng = will.lng () - now.lng (), aveAdd = ((Math.abs (addLat) + Math.abs (addLng)) / 2), unit = aveAdd < 10 ? aveAdd < 1 ? aveAdd < 0.1 ? aveAdd < 0.01 ? aveAdd < 0.001 ? aveAdd < 0.0001 ? 3 : 6 : 9 : 12 : 15 : 24 : 21, lat = addLat / unit, lng = addLng / unit; if (!((Math.abs (lat) > 0) || (Math.abs (lng) > 0))) return null; return { unit: unit, lat: lat, lng: lng }; }
 function markerMove (marker, unitLat, unitLng, unitCount, unit, callback) {if (unit > unitCount) {marker.setPosition (new google.maps.LatLng (marker.getPosition ().lat () + unitLat, marker.getPosition ().lng () + unitLng));clearTimeout (window.markerMoveTimer);window.markerMoveTimer = setTimeout (function () {markerMove (marker, unitLat, unitLng, unitCount + 1, unit, callback);}, 25);} else { if (callback) callback (marker); }}
@@ -14,12 +14,14 @@ function mapGo (map, will, callback) {var now = map.center;var Unit = getUnit (w
 $(function () {
   var $body = $('body');
   $('#m, #c').click (function () { $body.toggleClass ('s'); });
+  $('#ss, #sc').click (function () { $body.toggleClass ('ss'); });
   var $map = $('#mm');
   var $myPosition = $('#i');
   var $s = $('#s').click (function () {
     window.open ('https://www.facebook.com/sharer/sharer.php?u=' + window.location.href, '分享至臉書！', 'scrollbars=yes,resizable=yes,toolbar=no,location=yes,width=550,height=420,top=100,left=' + (window.screen ? Math.round(screen.width / 2 - 275) : 100));
   });
   var _map = null;
+  var _heatmap = null;
   var _markers = [];
   var _myMarker = null;
   var _polyline = null;
@@ -28,9 +30,10 @@ $(function () {
   var _isMove = false;
   var _v = 0;
   var _c = 0;
-  var _cl = 50;
+  var _cl = 20;
   var $length = $('#ll');
   var $mmm = $('#mmm');
+  var $ss = $('#ss');
 
   function circlePath (r) { return 'M 0 0 m -' + r + ', 0 '+ 'a ' + r + ',' + r + ' 0 1,0 ' + (r * 2) + ',0 ' + 'a ' + r + ',' + r + ' 0 1,0 -' + (r * 2) + ',0';}
   function calculateLength (points) { var size = Math.pow (10, 2); if (google.maps.geometry.spherical) { var l = Math.round (google.maps.geometry.spherical.computeLength (points) / 1000 * size) / size; if (l > 0) $length.text (l); }}
@@ -42,6 +45,51 @@ $(function () {
       data: { a: a, n: n },
       async: true, cache: false, dataType: 'json', type: 'POST',
     });
+  }
+  function loadHeatmap (text) {
+    if (_heatmap && ($ss.data ('val') < 0)) {
+      if (_heatmap) _heatmap.setData ([]);
+      return;
+    }
+    
+    $.ajax ({
+      url: _url3 + $ss.data ('val'),
+      data: {},
+      async: true, cache: false, dataType: 'json', type: 'GET',
+      beforeSend: function () {
+        if (_heatmap) {
+          _heatmap.setData ([]);
+          $ss.find ('span').text ('取得資料中..');
+        }
+      }
+    })
+    .done (function (result) {
+      if (!result.s) {
+        _heatmap.setData ([]);
+        $ss.data ('val', -1).find ('span').text ('不顯示分佈');
+        $ss.find ('>div>a[data-val="-1"]').addClass ('a').siblings ().removeClass ('a');
+        return ;
+      }
+      $ss.find ('span').text (text);
+
+      if (!_heatmap) {
+        $ss.addClass ('s').find ('a').click (function () {
+            $(this).addClass ('a').siblings ().removeClass ('a');
+            $ss.data ('val', $(this).data ('val'));
+            loadHeatmap ($(this).text ());
+          });
+        _heatmap = new google.maps.visualization.HeatmapLayer ({ map: _map });
+      }
+      _heatmap.setData (result.q.map (function (t) { return new google.maps.LatLng (t.a, t.n); }));
+    })
+    .fail (function (result) {
+      if (_heatmap) {
+        _heatmap.setData ([]);
+        $ss.data ('val', -1).find ('span').text ('不顯示分佈');
+        $ss.find ('>div>a[data-val="-1"]').addClass ('a').siblings ().removeClass ('a');
+      }
+    })
+    .complete (function (result) {});
   }
   function initialize () {
     _map = new google.maps.Map ($map.get (0), {
@@ -70,6 +118,23 @@ $(function () {
     });
     google.maps.event.addListener (_map, 'drag', function () {
       _isMove = true;
+    });
+
+    $myPosition.addClass ('s').click (function () {
+      $myPosition.text ('定位中.. 請稍候..');
+      navigator.geolocation.getCurrentPosition (function (location) {
+        $myPosition.text ('我的位置');
+
+        if (!_myMarker) _myMarker = new google.maps.Marker ({ map: _map, draggable: false, optimized: false});
+        _myMarker.setPosition (new google.maps.LatLng (location.coords.latitude, location.coords.longitude));
+        _myMarker.setIcon ({path: myPositionPath (30), strokeColor: 'rgba(174, 129, 255, .8)', strokeWeight: 3, fillColor: 'rgba(174, 129, 255, .5)', fillOpacity: 0.2});
+        _map.setCenter (new google.maps.LatLng (location.coords.latitude, location.coords.longitude));
+
+        setLoation (location.coords.latitude, location.coords.longitude);
+
+      }, function () {
+        $myPosition.remove ();
+      });
     });
 
     var reload = function () {
@@ -128,31 +193,16 @@ $(function () {
             return marker;
         }));
 
-
         if (!_polyline)
           _polyline = new google.maps.Polyline ({ map: _map, strokeColor: 'rgba(249, 39, 114, .35)', strokeWeight: 5 });
         _polyline.setPath (_markers.map (function (t) { return t.position; }));
+        
         if (!_isMove) mapGo (_map, new google.maps.LatLng (_latlngs[_latlngs.length - 1].lat, _latlngs[_latlngs.length - 1].lng));
+        
         $mmm.addClass ('h');
-        
         setTimeout (calculateLength.bind (this, _markers.map (function (t) { return t.position; })), 1800);
-        
-        $myPosition.addClass ('s').click (function () {
-          $myPosition.text ('定位中.. 請稍候..');
-          navigator.geolocation.getCurrentPosition (function (location) {
-            $myPosition.text ('我的位置');
 
-            if (!_myMarker) _myMarker = new google.maps.Marker ({ map: _map, draggable: false, optimized: false});
-            _myMarker.setPosition (new google.maps.LatLng (location.coords.latitude, location.coords.longitude));
-            _myMarker.setIcon ({path: myPositionPath (30), strokeColor: 'rgba(174, 129, 255, .8)', strokeWeight: 3, fillColor: 'rgba(174, 129, 255, .5)', fillOpacity: 0.2});
-            _map.setCenter (new google.maps.LatLng (location.coords.latitude, location.coords.longitude));
-
-            setLoation (location.coords.latitude, location.coords.longitude);
-
-          }, function () {
-            $myPosition.remove ();
-          });
-        });
+        loadHeatmap ('信徒目前分佈');
       });
     };
 
